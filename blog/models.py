27049dc592b66cd8ad70b46d4c7b763c4d926d46
robert <rhsu123@gmail.com>
2020-11-19 20:46:50 -0800

    putting this on hold one sec

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
