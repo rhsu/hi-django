430b4a0fd134f12d7378fe1eff43fab993f47bd1
robert <rhsu123@gmail.com>
2020-11-21 21:11:03 -0800

    ok cool. I think I figured out routing

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+def post_list(request):
+    data = { 'foo': 'bar' }
+    return JsonResponse(data)
